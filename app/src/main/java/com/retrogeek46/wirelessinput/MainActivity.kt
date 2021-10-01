509d512e615251794701a9e445cc2c7d6de71a71
RetroGeek46 <divyangpradeep@gmail.com>
2021-10-01 23:40:54 +0530

    Add server ip search (test)

--- app/src/main/java/com/retrogeek46/wirelessinput/MainActivity.kt
+++ app/src/main/java/com/retrogeek46/wirelessinput/MainActivity.kt
@@ -1,37 +1,70 @@
 package com.retrogeek46.wirelessinput
 
+import android.content.Context
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Debug
+import android.text.format.Formatter
+import android.text.format.Formatter.formatIpAddress
 import android.util.Log
 import android.view.View
-import android.widget.Button
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
 
-    var mGlobalSocket: io.socket.client.Socket? = null
+    private var mGlobalSocket: io.socket.client.Socket? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val keyA = findViewById<Button>(R.id.key_A)
-        val keyB = findViewById<Button>(R.id.key_B)
-        val keyC = findViewById<Button>(R.id.key33)
-
         SocketHandler.setSocket()
-        var mSocket = SocketHandler.getSocket()
+        val mSocket = SocketHandler.getSocket()
         mSocket.connect()
 
         mGlobalSocket = mSocket
 
+        getServerIP()
+
 //        keyA.setOnClickListener {
 //            mSocket.emit("fromAndroid", "keyA")
 //        }
+
+//        Log.i("wirelessInput",ipAddress)
     }
 
     fun sendMessage(view: View) {
         mGlobalSocket?.emit("fromAndroid", view.tag.toString())
+    }
+
+    fun getServerIP() {
+        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val ipAddress: String = Formatter.formatIpAddress(wifiManager.connectionInfo.ipAddress)
+
+        var ipPrefix:String = ipAddress.subSequence(0,ipAddress.lastIndexOf(".")).toString()
+
+        Log.i("wirelessInput", ipPrefix)
+
+        var serverIp:String = ""
+
+        val queue = Volley.newRequestQueue(this)
+        for (i in 0..250) {
+            val url = "$ipPrefix.$i"
+            val stringRequest = StringRequest(
+                Request.Method.GET, url,
+                { response ->
+                    Log.i("wirelessInput","Response is: ${response.substring(0, 500)} for $url")
+                    serverIp = url
+                },
+                { Log.i("wirelessInput","That didn't work for $url") })
+            queue.add(stringRequest)
+        }
     }
 }
